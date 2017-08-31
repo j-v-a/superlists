0265f10139b94fbc593486058d7adaf1aa6a18f5
j-v-a <jvanameijden@gmail.com>
2017-08-31 11:06:16 +0200

    new app for accounts, enable full console logging

--- superlists/settings.py
+++ superlists/settings.py
@@ -120,3 +120,23 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os. path.abspath(os.path.join(BASE_DIR, '../static'))
+
+
+# See all exceptions in the console
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
